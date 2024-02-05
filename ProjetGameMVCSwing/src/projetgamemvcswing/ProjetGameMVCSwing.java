e108b8d2b7078b640a3b75c0209958e673746808
Romain Andres <romain.andres@etu.unicaen.fr>
2024-02-05 22:28:29 +0100

    ajout d'un projet netbean java ant vierge avec un fichier principal qui fait hello world

---
+++ ProjetGameMVCSwing/src/projetgamemvcswing/ProjetGameMVCSwing.java
@@ -0,0 +1,16 @@
+package projetgamemvcswing;
+
+/**
+ *
+ * @author romain
+ */
+public class ProjetGameMVCSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+    
+}
