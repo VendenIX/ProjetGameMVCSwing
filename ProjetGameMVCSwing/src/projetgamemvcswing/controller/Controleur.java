42c1cd0befa6bb0e091ba6cdb205a7da99fe922c
OROU-GUIDOU <amirathfarah@gmail.com>
2024-02-20 10:47:26 +0100

    ton commit

---
+++ ProjetGameMVCSwing/src/projetgamemvcswing/controller/Controleur.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetgamemvcswing.controller;
+
+/**
+ *
+ * @author <a href="mailto:22012235@etu.unicaen.fr">Amirath Fara OROU-GUIDOU</a>
+ * 
+ * @version 1.0
+ * 
+ * Controleur de l'application
+ */
+public class Controleur {
+
+    
+    
+}
